f6607ac2a65e34084011f04903e4ba10522a0c5d
Dwi Sasongko S <ruckuus@gmail.com>
2014-10-16 21:52:18 +0800

    Views, with a bit modification for index and individual view handler.

--- wiki.go
+++ wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "strings"
   "io/ioutil"
   "net/http"
 )
@@ -11,13 +12,24 @@ type Page struct {
   Body  []byte
 }
 
+var post_dir string = "_posts/"
+var view_slug string = "/view/"
+var debug_enabled int = 1
+
+func debug(msg string) {
+  if debug_enabled != 0 {
+    fmt.Println(msg)
+  }
+}
+
 func (p *Page) save() error {
   filename := p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
+
 }
 
 func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
+  filename := post_dir + title + ".txt"
   body, err := ioutil.ReadFile(filename)
 
   if err != nil {
@@ -27,19 +39,30 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  debug("Inside indexHandler")
+  files, _ := ioutil.ReadDir(post_dir)
+  fmt.Fprintf(w, "<h1>Posts</h1>")
+  for _, f := range files {
+    post := strings.Split(f.Name(), ".")
+    fmt.Fprintf(w, "<h2><a href=\"%s%s\">%s</a></h2>", view_slug, string(post[0]), string(post[0]))
+  }
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  page := r.URL.Path[1:]
-  p, _ := loadPage(page)
+  debug("Inside viewHandler")
+  title := r.URL.Path[len("/view/"):]
+  p, _ := loadPage(title)
 
   if p != nil {
     fmt.Fprintf(w, "<h1>%s</h1><body>%s</body>", string(p.Title), string(p.Body))
   } else {
     fmt.Fprintf(w, "Internal Server Error")
   }
-
 }
 
 func main() {
-  http.HandleFunc("/", viewHandler)
+  http.HandleFunc("/", indexHandler)
+  http.HandleFunc(view_slug, viewHandler)
   http.ListenAndServe(":8080", nil)
 }
